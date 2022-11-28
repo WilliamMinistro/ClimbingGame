37e79b5d24072fd8e4833adf6aa9d7ad8e8c3dfc
williamministro <ministrobilly@gmail.com>
2022-11-27 19:17:18 -0500

    Added a point system. Still in progress, but working.

--- Assets/Scripts/RandomGenerator.cs
+++ Assets/Scripts/RandomGenerator.cs
@@ -5,25 +5,27 @@ using UnityEngine.UI;
 
 public class RandomGenerator : MonoBehaviour
 {
-    public GameObject spikes;
-    public GameObject spikeClone;
-    public GameObject SpinningSpikeClone;
-    public GameObject FireClone;
-    public GameObject SpinningSpikeClone2;
-    public GameObject bandageClone;
-    public GameObject MainCamera;
+//    public GameObject spikes;
+//    public GameObject spikeClone;
+//    public GameObject SpinningSpikeClone;
+//    public GameObject FireClone;
+//    public GameObject SpinningSpikeClone2;
+//    public GameObject bandageClone;
+//    public GameObject MainCamera;
     public Text pointText;
-    public Text pointGameOver;
-    public Text healthText;
+//    public Text pointGameOver;
+//    public Text healthText;
     public static float respawnTime = 2f;
     private Vector2 bounds;
     public static int pointCount;
     public GameObject balloon;
-    private int yValue = 6;
-    private int yValueRight = 6;
-    private int yValueBand = 6;
+//    private int yValue = 6;
+//    private int yValueRight = 6;
+//    private int yValueBand = 6;
     public int randVariable, randVariableRight;
     bool isSpikeCol;
+    float score;
+    double score_rounded;
 
 
     System.Random random = new System.Random();
@@ -31,7 +33,8 @@ public class RandomGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(objectWave());
+        //StartCoroutine(objectWave());
+
     }
 
     // Update is called once per frame
@@ -40,116 +43,116 @@ public class RandomGenerator : MonoBehaviour
         points();
     }
 
-    private void spawnL()
-    {
-        int whichObj = random.Next(1, 4);
+//    private void spawnL()
+//    {
+//        int whichObj = random.Next(1, 4);
+//
+//        if (whichObj == 1)
+//        {
+//            randVariable = random.Next(6, 10);
+//
+//            yValue = yValue + randVariable;
+//            Instantiate(spikeClone, new Vector3(-1.95f, (yValue), 0f), Quaternion.Euler(0, 0, -90));
+//        }
+//
+//        if (whichObj == 2)
+//        {
+//            int randSpikeCol = random.Next(1, 3);
+//
+//            if (randSpikeCol == 1)
+//            {
+//                randVariable = random.Next(7, 9);
+//                yValue = yValue + randVariable;
+//                Instantiate(SpinningSpikeClone, new Vector3(-2.45f, (yValue), 0f), Quaternion.Euler(0, 0, -25.445f));
+//            }
+//            if (randSpikeCol == 2)
+//            {
+//                randVariable = random.Next(8, 9);
+//                yValue = yValue + randVariable;
+//                Instantiate(SpinningSpikeClone, new Vector3(-1.75f, (yValue), 0f), Quaternion.Euler(0, 0, 90f));
+//                isSpikeCol = true;
+//            }
+//        }
+//        if(whichObj == 3)
+//        {
+//            randVariable = random.Next(6, 10);
+//            yValue = yValue + randVariable;
+//            Instantiate(FireClone, new Vector3(-1.62f, (yValue), 0f), Quaternion.Euler(0, 0, 0));
+//        }
+//    }
+//    private void spawnR()
+//    {
+//        int whichObj2 = random.Next(1, 4);
+//
+//        if (isSpikeCol == false)
+//        {
+//            if (whichObj2 == 1)
+//            {
+//                randVariableRight = random.Next(6, 10);
+//                yValueRight = yValueRight + randVariableRight;
+//                Instantiate(spikeClone, new Vector3(1.95f, (yValueRight), 0f), Quaternion.Euler(0, 0, 90));
+//            }
+//            if(whichObj2 == 2)
+//            {
+//                randVariableRight = random.Next(6, 10);
+//                yValueRight = yValueRight + randVariableRight;
+//                Instantiate(FireClone, new Vector3(1.62f, (yValueRight), 0f), Quaternion.Euler(0, 0, 180));
+//            }
+//            if (whichObj2 == 3)
+//            {
+//                randVariableRight = random.Next(6, 10);
+//                yValueRight = yValueRight + randVariableRight;
+//                Instantiate(SpinningSpikeClone2, new Vector3(3.11f, (yValueRight), 0f), Quaternion.Euler(0, 0, 90));
+//            }
+//        }
+//        else
+//        {
+//            randVariableRight = random.Next(7, 10);
+//            yValueRight = yValueRight + randVariableRight;
+//        }
+//
+//        isSpikeCol = false;
+//
+//    }
 
-        if (whichObj == 1)
-        {
-            randVariable = random.Next(6, 10);
-  
-            yValue = yValue + randVariable;
-            Instantiate(spikeClone, new Vector3(-1.95f, (yValue), 0f), Quaternion.Euler(0, 0, -90));
-        }
+//    private void spawnBandage()
+//    {
+//        int bandagePlacement = random.Next(1, 4);
+//
+//        if(bandagePlacement == 2)
+//        {
+//            int randYVal = random.Next(6, 10);
+//            yValueBand = yValueBand + randYVal;
+//            Instantiate(bandageClone, new Vector3(0f, yValueBand, 0f), Quaternion.Euler(0, 0, 0));
+//        }
+//        else
+//        {
+//            int randYVal = random.Next(6, 10);
+//            yValueBand = yValueBand + randYVal;
+//        }
+//
+//    }
 
-        if (whichObj == 2)
-        {
-            int randSpikeCol = random.Next(1, 3);
-
-            if (randSpikeCol == 1)
-            {
-                randVariable = random.Next(7, 9);
-                yValue = yValue + randVariable;
-                Instantiate(SpinningSpikeClone, new Vector3(-2.45f, (yValue), 0f), Quaternion.Euler(0, 0, -25.445f));
-            }
-            if (randSpikeCol == 2)
-            {
-                randVariable = random.Next(8, 9);
-                yValue = yValue + randVariable;
-                Instantiate(SpinningSpikeClone, new Vector3(-1.75f, (yValue), 0f), Quaternion.Euler(0, 0, 90f));
-                isSpikeCol = true;
-            }
-        }
-        if(whichObj == 3)
-        {
-            randVariable = random.Next(6, 10);
-            yValue = yValue + randVariable;
-            Instantiate(FireClone, new Vector3(-1.62f, (yValue), 0f), Quaternion.Euler(0, 0, 0));
-        }
-    }
-    private void spawnR()
-    {
-        int whichObj2 = random.Next(1, 4);
-
-        if (isSpikeCol == false)
-        {
-            if (whichObj2 == 1)
-            {
-                randVariableRight = random.Next(6, 10);
-                yValueRight = yValueRight + randVariableRight;
-                Instantiate(spikeClone, new Vector3(1.95f, (yValueRight), 0f), Quaternion.Euler(0, 0, 90));
-            }
-            if(whichObj2 == 2)
-            {
-                randVariableRight = random.Next(6, 10);
-                yValueRight = yValueRight + randVariableRight;
-                Instantiate(FireClone, new Vector3(1.62f, (yValueRight), 0f), Quaternion.Euler(0, 0, 180));
-            }
-            if (whichObj2 == 3)
-            {
-                randVariableRight = random.Next(6, 10);
-                yValueRight = yValueRight + randVariableRight;
-                Instantiate(SpinningSpikeClone2, new Vector3(3.11f, (yValueRight), 0f), Quaternion.Euler(0, 0, 90));
-            }    
-        }
-        else
-        {
-            randVariableRight = random.Next(7, 10);
-            yValueRight = yValueRight + randVariableRight;
-        }
-
-        isSpikeCol = false;
-        
-    }
-
-    private void spawnBandage()
-    {
-        int bandagePlacement = random.Next(1, 4);
-
-        if(bandagePlacement == 2)
-        {
-            int randYVal = random.Next(6, 10);
-            yValueBand = yValueBand + randYVal;
-            Instantiate(bandageClone, new Vector3(0f, yValueBand, 0f), Quaternion.Euler(0, 0, 0));
-        }
-        else
-        {
-            int randYVal = random.Next(6, 10);
-            yValueBand = yValueBand + randYVal;
-        }
-
-    }
-
-
-    IEnumerator objectWave()
-    {
-        while (true)
-        {
-
-            spawnL();
-            spawnR();
-            spawnBandage();
-            yield return new WaitForSeconds(respawnTime);
-        }
-    }
+//
+//    IEnumerator objectWave()
+//    {
+//        while (true)
+//        {
+//
+//            spawnL();
+//            spawnR();
+//            spawnBandage();
+//            yield return new WaitForSeconds(respawnTime);
+//        }
+//    }
 
     public void points()
     {
-        Vector3 BalloonY = balloon.transform.position;
-        pointCount = (int)BalloonY.y + 3;
-        pointText.text = "POINTS: " + pointCount;
-        healthText.text = "HEALTH: " + HealthController.Health;
-        pointGameOver.text = "" + pointCount;
+        score = score + 0.0003f;
+        score_rounded = System.Math.Round(score, 1);
+        pointText.text = score_rounded + "m";
+//        healthText.text = "HEALTH: " + HealthController.Health;
+//        pointGameOver.text = "" + pointCount;
     }
     
     public void PointChanger()
