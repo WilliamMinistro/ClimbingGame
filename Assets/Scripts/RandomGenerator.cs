88439516af89c586418f72588338e030c8daddb9
williamministro <ministrobilly@gmail.com>
2022-11-28 16:57:58 -0500

    Added random generation of fireballs and rocks to each lane. Also added logic to prevent all 3 lanes from being blocked

--- Assets/Scripts/RandomGenerator.cs
+++ Assets/Scripts/RandomGenerator.cs
@@ -29,9 +29,34 @@ public class RandomGenerator : MonoBehaviour
     IEnumerator objectWave()
     {
         while(true){
-            spawnL();
-            spawnR();
-            spawnM();
+            int whichLane = random.Next(1,7);
+            if(whichLane == 1)
+            {
+                spawnL();
+            }
+            if(whichLane == 2)
+            {
+                spawnR();
+            }
+            if(whichLane == 3)
+            {
+                spawnM();
+            }
+            if(whichLane == 4)
+            {
+                spawnL();
+                spawnR();
+            }
+            if(whichLane == 5)
+            {
+                spawnL();
+                spawnM();
+            }
+            if(whichLane == 6)
+            {
+                spawnR();
+                spawnM();
+            }
 //            spawnBandage();
 //            yield return new WaitForSeconds(respawnTime);
             yield return new WaitForSeconds(respawnTime);
@@ -41,82 +66,49 @@ public class RandomGenerator : MonoBehaviour
     private void spawnL()
     {
         int willSpawn = random.Next(1,10);
-        if(willSpawn == 2)
+        if(willSpawn == 1)
         {
             Instantiate(stalagmite, new Vector3(-1.3f, 6, 10), Quaternion.identity);
+        }
+        if(willSpawn == 2)
+        {
+            Instantiate(fireball, new Vector3(-1.3f, 6, 10), Quaternion.identity);
+        }
+        if(willSpawn == 3)
+        {
+            Instantiate(rock, new Vector3(-1.3f, 6, 10), Quaternion.identity);
         }
     }
     private void spawnM()
     {
         int willSpawn = random.Next(1,10);
-        if(willSpawn == 2)
+        if(willSpawn == 1)
         {
             Instantiate(stalagmite, new Vector3(0f, 6, 10), Quaternion.identity);
+        }
+        if(willSpawn == 2)
+        {
+            Instantiate(fireball, new Vector3(0f, 6, 10), Quaternion.identity);
+        }
+        if(willSpawn == 3)
+        {
+            Instantiate(rock, new Vector3(0f, 6, 10), Quaternion.identity);
         }
     }
     private void spawnR()
     {
         int willSpawn = random.Next(1,10);
-        if(willSpawn == 2)
+        if(willSpawn == 1)
         {
             Instantiate(stalagmite, new Vector3(1.3f, 6, 10), Quaternion.identity);
         }
+        if(willSpawn == 2)
+        {
+            Instantiate(fireball, new Vector3(1.3f, 6, 10), Quaternion.identity);
+        }
+        if(willSpawn == 3)
+        {
+            Instantiate(rock, new Vector3(1.3f, 6, 10), Quaternion.identity);
+        }
     }
-
-//    private void spawnR()
-//    {
-//        int whichObj2 = random.Next(1, 4);
-//
-//        if (isSpikeCol == false)
-//        {
-//            if (whichObj2 == 1)
-//            {
-//                randVariableRight = random.Next(6, 10);
-//                yValueRight = yValueRight + randVariableRight;
-//                Instantiate(spikeClone, new Vector3(1.95f, (yValueRight), 0f), Quaternion.Euler(0, 0, 90));
-//            }
-//            if(whichObj2 == 2)
-//            {
-//                randVariableRight = random.Next(6, 10);
-//                yValueRight = yValueRight + randVariableRight;
-//                Instantiate(FireClone, new Vector3(1.62f, (yValueRight), 0f), Quaternion.Euler(0, 0, 180));
-//            }
-//            if (whichObj2 == 3)
-//            {
-//                randVariableRight = random.Next(6, 10);
-//                yValueRight = yValueRight + randVariableRight;
-//                Instantiate(SpinningSpikeClone2, new Vector3(3.11f, (yValueRight), 0f), Quaternion.Euler(0, 0, 90));
-//            }
-//        }
-//        else
-//        {
-//            randVariableRight = random.Next(7, 10);
-//            yValueRight = yValueRight + randVariableRight;
-//        }
-//
-//        isSpikeCol = false;
-//
-//    }
-
-//    private void spawnBandage()
-//    {
-//        int bandagePlacement = random.Next(1, 4);
-//
-//        if(bandagePlacement == 2)
-//        {
-//            int randYVal = random.Next(6, 10);
-//            yValueBand = yValueBand + randYVal;
-//            Instantiate(bandageClone, new Vector3(0f, yValueBand, 0f), Quaternion.Euler(0, 0, 0));
-//        }
-//        else
-//        {
-//            int randYVal = random.Next(6, 10);
-//            yValueBand = yValueBand + randYVal;
-//        }
-//
-//    }
-
-//
-
-
 }
