df74dbbef21f273d79ff3c1c75d52b1286d48e46
williamministro <ministrobilly@gmail.com>
2023-05-02 16:20:09 -0400

    Revamped coin generation script to allow for seperate lane cooldown, and changed to where the coins are better evenly distributed. Changed rock rotation script to allow for seperate rotation directions and speeds that are randomized, which is purely aesthetic.

--- Assets/Scripts/CoinGeneration.cs
+++ Assets/Scripts/CoinGeneration.cs
@@ -6,8 +6,10 @@ public class CoinGeneration : MonoBehaviour
 {
     public static float speed = -.001f;
     public GameObject coin;
-    public float respawnTime = 3;
+    public float respawnTime = 6;
     bool isCreated;
+    private float coinSpawnProbability = 0.5f; // Set this according to the desired coin spawn probability
+    public float[] laneCooldowns = new float[6];
 
     System.Random random = new System.Random();
     // Start is called before the first frame update
@@ -19,109 +21,91 @@ public class CoinGeneration : MonoBehaviour
     IEnumerator coinWave()
     {
         while(true){
-            int whichLane = random.Next(1,7);
-            if(whichLane == 1)
+            for (int i = 0; i <= 5; i++)
             {
-                spawnL();
+                if (laneCooldowns[i] <= 0)
+                {
+                    switch (i)
+                    {
+                        case 1:
+                            spawnL();
+                            break;
+                        case 2:
+                            spawnR();
+                            break;
+                        case 3:
+                            spawnM();
+                            break;
+                        case 4:
+                            spawnL();
+                            spawnR();
+                            break;
+                        case 5:
+                            spawnL();
+                            spawnM();
+                            break;
+                        case 6:
+                            spawnR();
+                            spawnM();
+                            break;
+                    }
+                }
             }
-            if(whichLane == 2)
-            {
-                spawnR();
-            }
-            if(whichLane == 3)
-            {
-                spawnM();
-            }
-            if(whichLane == 4)
-            {
-                spawnL();
-                spawnR();
-            }
-            if(whichLane == 5)
-            {
-                spawnL();
-                spawnM();
-            }
-            if(whichLane == 6)
-            {
-                spawnR();
-                spawnM();
-            }
-            yield return new WaitForSeconds(respawnTime);
+            yield return null;
         }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        for (int i = 0; i < laneCooldowns.Length; i++)
+        {
+            if (laneCooldowns[i] > 0)
+            {
+                laneCooldowns[i] -= Time.fixedDeltaTime;
+            }
+            else
+            {
+                laneCooldowns[i] = 0;
+            }
+        }
     }
     private void spawnL()
     {
-            int coinSpawn = random.Next(1,7);
-            if(coinSpawn == 4)
-            {
-                int howManyCoins = random.Next(1,4);
-                if(howManyCoins == 1)
-                {
-                    Instantiate(coin, new Vector3(-1.3f, 6, 10), Quaternion.identity);
-                }
-                if(howManyCoins == 2)
-                {
-                    Instantiate(coin, new Vector3(-1.3f, 6, 10), Quaternion.identity);
-                    Instantiate(coin, new Vector3(-1.3f, 8, 10), Quaternion.identity);
-                }
-                if(howManyCoins == 3)
-                {
-                    Instantiate(coin, new Vector3(-1.3f, 6, 10), Quaternion.identity);
-                    Instantiate(coin, new Vector3(-1.3f, 8, 10), Quaternion.identity);
-                    Instantiate(coin, new Vector3(-1.3f, 10, 10), Quaternion.identity);
-                }
-            }
+        if (laneCooldowns[0] <= 0 && random.NextDouble() <= coinSpawnProbability)
+        {
+            SpawnCoins(-1.3f);
+            laneCooldowns[0] = respawnTime;
+        }
     }
     private void spawnR()
     {
-            int coinSpawn = random.Next(1,7);
-            if(coinSpawn == 4)
-            {
-                int howManyCoins = random.Next(1,4);
-                if(howManyCoins == 1)
-                {
-                    Instantiate(coin, new Vector3(1.3f, 6, 10), Quaternion.identity);
-                }
-                if(howManyCoins == 2)
-                {
-                    Instantiate(coin, new Vector3(1.3f, 6, 10), Quaternion.identity);
-                    Instantiate(coin, new Vector3(1.3f, 8, 10), Quaternion.identity);
-                }
-                if(howManyCoins == 3)
-                {
-                    Instantiate(coin, new Vector3(1.3f, 6, 10), Quaternion.identity);
-                    Instantiate(coin, new Vector3(1.3f, 8, 10), Quaternion.identity);
-                    Instantiate(coin, new Vector3(1.3f, 10, 10), Quaternion.identity);
-                }
-            }
+        if (laneCooldowns[1] <= 0 && random.NextDouble() <= coinSpawnProbability)
+        {
+            SpawnCoins(1.3f);
+            laneCooldowns[1] = respawnTime;
+        }
     }
     private void spawnM()
     {
-            int coinSpawn = random.Next(1,7);
-            if(coinSpawn == 4)
+        if(laneCooldowns[2] <= 0 && random.NextDouble() <= coinSpawnProbability)
+        {
+            SpawnCoins(0f);
+            laneCooldowns[2] = respawnTime;
+        }
+    }
+
+    private void SpawnCoins(float xPosition)
+    {
+        int willSpawn = random.Next(0, 3);
+        if(willSpawn == 2)
+        {
+            int howManyCoins = random.Next(1, 4);
+            for (int i = 0; i< howManyCoins; i++)
             {
-                int howManyCoins = random.Next(1,4);
-                if(howManyCoins == 1)
-                {
-                    Instantiate(coin, new Vector3(0f, 6, 10), Quaternion.identity);
-                }
-                if(howManyCoins == 2)
-                {
-                    Instantiate(coin, new Vector3(0f, 6, 10), Quaternion.identity);
-                    Instantiate(coin, new Vector3(0f, 8, 10), Quaternion.identity);
-                }
-                if(howManyCoins == 3)
-                {
-                    Instantiate(coin, new Vector3(0f, 6, 10), Quaternion.identity);
-                    Instantiate(coin, new Vector3(0f, 8, 10), Quaternion.identity);
-                    Instantiate(coin, new Vector3(0f, 10, 10), Quaternion.identity);
-                }
+                float yPos = 6 + (i * 2);
+                Instantiate(coin, new Vector3(xPosition, yPos, 10), Quaternion.identity);
             }
+        }
     }
 }
--- Assets/Scripts/RockRotation.cs
+++ Assets/Scripts/RockRotation.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class RockRotation : MonoBehaviour
 {
     System.Random rand = new System.Random();
+    float rotationSpeed;
+    int rotationDirection;
     // Start is called before the first frame update
     void Start()
     {
-
+        rotationSpeed = (float)rand.NextDouble() * 40 + 10; // Random speed between 10 and 50
+        rotationDirection = rand.Next(0, 2) == 0 ? -1 : 1; // Random direction (clockwise or counterclockwise)
     }
 
     // Update is called once per frame
     void Update()
     {
-            transform.Rotate(new Vector3(0,0,20*(Time.deltaTime)));
+            transform.Rotate(new Vector3(0,0,rotationSpeed * rotationDirection * (Time.deltaTime)));
     }
 }
